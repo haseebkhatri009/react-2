5332e293e5ee98e2349d06d59103310d78e606e7
Haseeb Khatri <153817119+haseebkhatri009@users.noreply.github.com>
2024-10-22 14:33:59 +0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <>
+    <div class="intro">
+      <h1>Welcome to my website!</h1>
+    </div>
+    <p class="summary">
+      You can find my thoughts here.
+      <br></br>
+      <b>And</b> <i>pictures</i> of scientists!
+    </p>
+    </>
+  </StrictMode>,
+)
